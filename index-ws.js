7ca954bc0d504dc626c35c1d556df50134bf2a5a
Nick Gusto <nicholas.gusto@gmail.com>
2023-07-06 15:41:43 -0700

    Add SQLite functionality to our web server

--- index-ws.js
+++ index-ws.js
@@ -10,6 +10,15 @@ app.get('/', function(req, res) {
 server.on('request', app);
 server.listen(3000, function() { console.log("Server listening on port 3000")});
 
+process.on("SIGINT", () => {
+    wss.clients.forEach(function each(client) {
+        client.close();
+    });
+    server.close(() => {
+        shutdownDB();
+    });
+})
+
 // Begin Websockets
 const WebSocketServer = require('ws').Server;
 
@@ -25,6 +34,10 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server!');
      }
 
+     db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+     `);
+
      ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);
         console.log('Client disconnected');
@@ -36,4 +49,32 @@ wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
         client.send(data);
     });
+}
+
+// End Websockets
+
+// Begin Database
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `)
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log("Shutting down database");
+    db.close();
 }
