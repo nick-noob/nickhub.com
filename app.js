145d02bc7806216ef3206939641fa41954e3fb79
Nick Gusto <nicholas.gusto@gmail.com>
2023-07-07 13:49:55 -0700

    Add Tailwind CSS and nickhub images

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 http.createServer(function (req, res) {
-res.write("On the way to being a full stack engineer!");
-res.end();
-	
-}
-).listen(3000);
+ if (req.url === "/") {
+	 fs.readFile(path.join(__dirname, 'index.html'), function(err, data) {
+		 if (err) {
+			 res.writeHead(404);
+			 res.end(JSON.stringify(err));
+			 return;
+		 }
+		 res.writeHead(200);
+		 res.end(data);
+	});
+	}	
+}).listen(3000);
 
 console.log("Server started on port 3000");
